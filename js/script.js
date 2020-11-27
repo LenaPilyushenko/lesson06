0d625a5629422ef76d0f5934ee74a91a3608f4f0
Lena Pilyushenko <elena_pilyushenko@mail.ru>
2020-11-27 11:40:38 +0300

    feat(js/script.js):new fit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+function randomInt(min, max) {
+    // случайное число от min до (max+1)
+    let rand = min + Math.random() * (max - min);
+    return Math.floor(rand);
+}
+
+const isNumber = function(userNumber) {
+    return !isNaN(parseFloat(userNumber)) && isFinite(userNumber);
+};
+  
+const randomNumber = randomInt(1,100);
+
+function play() {
+    let userNumber = prompt('Угадай число от 1 до 100?');
+    if (+userNumber === randomNumber) {
+        alert('Поздравляю, Вы угадали!!!');
+        return;
+    }  if (userNumber === null) { 
+        alert('Игра окончена');
+        return;
+    } if (!isNumber(userNumber)) {
+        alert('Введи число!');
+        play();
+    } if (userNumber < randomNumber) {
+        alert('Загаданное число больше');
+        play();
+    } else {
+        alert('Загаданное число меньше');
+        play();
+    }    
+}
+
+play();
